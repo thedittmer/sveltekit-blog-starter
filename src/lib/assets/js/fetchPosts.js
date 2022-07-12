815fdacc9e61fcf3e8d84eb509d974c501e58521
Jason Dittmer <jason@zrimarketing.com>
2022-07-12 12:41:16 -0500

    hard coded posts

--- src/lib/assets/js/fetchPosts.js
+++ src/lib/assets/js/fetchPosts.js
@@ -1,13 +1,597 @@
 import { postsPerPage } from '$lib/config'
 
 const fetchPosts = async ({ offset = 0, limit = postsPerPage, category = '' } = {}) => {
-  const posts = await Promise.all(
-    Object.entries(import.meta.glob('../../posts/*.md')).map(async ([path, resolver]) => {
-      const { metadata } = await resolver()
-      const slug = path.split('/').pop().slice(0, -3)
-      return { ...metadata, slug }
-    })
-  )
+  const posts = [
+    {
+      "title": "This is the first post",
+      "slug": "mdsvex-component-example",
+      "excerpt": "This post demonstrates how to include a Svelte component in a Markdown post.",
+      "coverImage": "/images/jerry-zhang-ePpaQC2c1xA-unsplash.jpg",
+      "coverWidth": 16,
+      "coverHeight": 9,
+      "date": "2021-12-01",
+      "categories": [
+        "sveltekit",
+        "markdown",
+        "svelte"
+      ]
+    },
+    {
+      "title": "Syntax highlighting with mdsvex",
+      "slug": "syntax-highlighting-example",
+      "excerpt": "This post shows you how syntax highlighting works here.",
+      "coverImage": "/images/linus-nylund-Q5QspluNZmM-unsplash.jpg",
+      "coverWidth": 16,
+      "coverHeight": 9,
+      "date": "2021-12-01",
+      "categories": [
+        "sveltekit",
+        "web",
+        "css",
+        "markdown"
+      ]
+    },
+    {
+      "title": "Automatic heading links in mdsvex",
+      "slug": "heading-links-example",
+      "excerpt": "Check out how heading links work with this starter in this post.",
+      "coverImage": "/images/jefferson-santos-fCEJGBzAkrU-unsplash.jpg",
+      "coverWidth": 16,
+      "coverHeight": 9,
+      "date": "2021-10-26",
+      "categories": [
+        "sveltekit",
+        "markdown"
+      ]
+    },
+    {
+      "title": "A Markdown post with a Svelte component",
+      "slug": "mdsvex-component-example",
+      "excerpt": "This post demonstrates how to include a Svelte component in a Markdown post.",
+      "coverImage": "/images/jerry-zhang-ePpaQC2c1xA-unsplash.jpg",
+      "coverWidth": 16,
+      "coverHeight": 9,
+      "date": "2021-12-01",
+      "categories": [
+        "sveltekit",
+        "markdown",
+        "svelte"
+      ]
+    },
+    {
+      "title": "Syntax highlighting with mdsvex",
+      "slug": "syntax-highlighting-example",
+      "excerpt": "This post shows you how syntax highlighting works here.",
+      "coverImage": "/images/linus-nylund-Q5QspluNZmM-unsplash.jpg",
+      "coverWidth": 16,
+      "coverHeight": 9,
+      "date": "2021-12-01",
+      "categories": [
+        "sveltekit",
+        "web",
+        "css",
+        "markdown"
+      ]
+    },
+    {
+      "title": "Automatic heading links in mdsvex",
+      "slug": "heading-links-example",
+      "excerpt": "Check out how heading links work with this starter in this post.",
+      "coverImage": "/images/jefferson-santos-fCEJGBzAkrU-unsplash.jpg",
+      "coverWidth": 16,
+      "coverHeight": 9,
+      "date": "2021-10-26",
+      "categories": [
+        "sveltekit",
+        "markdown"
+      ]
+    },
+    {
+      "title": "A Markdown post with a Svelte component",
+      "slug": "mdsvex-component-example",
+      "excerpt": "This post demonstrates how to include a Svelte component in a Markdown post.",
+      "coverImage": "/images/jerry-zhang-ePpaQC2c1xA-unsplash.jpg",
+      "coverWidth": 16,
+      "coverHeight": 9,
+      "date": "2021-12-01",
+      "categories": [
+        "sveltekit",
+        "markdown",
+        "svelte"
+      ]
+    },
+    {
+      "title": "Syntax highlighting with mdsvex",
+      "slug": "syntax-highlighting-example",
+      "excerpt": "This post shows you how syntax highlighting works here.",
+      "coverImage": "/images/linus-nylund-Q5QspluNZmM-unsplash.jpg",
+      "coverWidth": 16,
+      "coverHeight": 9,
+      "date": "2021-12-01",
+      "categories": [
+        "sveltekit",
+        "web",
+        "css",
+        "markdown"
+      ]
+    },
+    {
+      "title": "Automatic heading links in mdsvex",
+      "slug": "heading-links-example",
+      "excerpt": "Check out how heading links work with this starter in this post.",
+      "coverImage": "/images/jefferson-santos-fCEJGBzAkrU-unsplash.jpg",
+      "coverWidth": 16,
+      "coverHeight": 9,
+      "date": "2021-10-26",
+      "categories": [
+        "sveltekit",
+        "markdown"
+      ]
+    },
+    {
+      "title": "A Markdown post with a Svelte component",
+      "slug": "mdsvex-component-example",
+      "excerpt": "This post demonstrates how to include a Svelte component in a Markdown post.",
+      "coverImage": "/images/jerry-zhang-ePpaQC2c1xA-unsplash.jpg",
+      "coverWidth": 16,
+      "coverHeight": 9,
+      "date": "2021-12-01",
+      "categories": [
+        "sveltekit",
+        "markdown",
+        "svelte"
+      ]
+    },
+    {
+      "title": "Syntax highlighting with mdsvex",
+      "slug": "syntax-highlighting-example",
+      "excerpt": "This post shows you how syntax highlighting works here.",
+      "coverImage": "/images/linus-nylund-Q5QspluNZmM-unsplash.jpg",
+      "coverWidth": 16,
+      "coverHeight": 9,
+      "date": "2021-12-01",
+      "categories": [
+        "sveltekit",
+        "web",
+        "css",
+        "markdown"
+      ]
+    },
+    {
+      "title": "Automatic heading links in mdsvex",
+      "slug": "heading-links-example",
+      "excerpt": "Check out how heading links work with this starter in this post.",
+      "coverImage": "/images/jefferson-santos-fCEJGBzAkrU-unsplash.jpg",
+      "coverWidth": 16,
+      "coverHeight": 9,
+      "date": "2021-10-26",
+      "categories": [
+        "sveltekit",
+        "markdown"
+      ]
+    },
+    {
+      "title": "A Markdown post with a Svelte component",
+      "slug": "mdsvex-component-example",
+      "excerpt": "This post demonstrates how to include a Svelte component in a Markdown post.",
+      "coverImage": "/images/jerry-zhang-ePpaQC2c1xA-unsplash.jpg",
+      "coverWidth": 16,
+      "coverHeight": 9,
+      "date": "2021-12-01",
+      "categories": [
+        "sveltekit",
+        "markdown",
+        "svelte"
+      ]
+    },
+    {
+      "title": "Syntax highlighting with mdsvex",
+      "slug": "syntax-highlighting-example",
+      "excerpt": "This post shows you how syntax highlighting works here.",
+      "coverImage": "/images/linus-nylund-Q5QspluNZmM-unsplash.jpg",
+      "coverWidth": 16,
+      "coverHeight": 9,
+      "date": "2021-12-01",
+      "categories": [
+        "sveltekit",
+        "web",
+        "css",
+        "markdown"
+      ]
+    },
+    {
+      "title": "Automatic heading links in mdsvex",
+      "slug": "heading-links-example",
+      "excerpt": "Check out how heading links work with this starter in this post.",
+      "coverImage": "/images/jefferson-santos-fCEJGBzAkrU-unsplash.jpg",
+      "coverWidth": 16,
+      "coverHeight": 9,
+      "date": "2021-10-26",
+      "categories": [
+        "sveltekit",
+        "markdown"
+      ]
+    },
+    {
+      "title": "A Markdown post with a Svelte component",
+      "slug": "mdsvex-component-example",
+      "excerpt": "This post demonstrates how to include a Svelte component in a Markdown post.",
+      "coverImage": "/images/jerry-zhang-ePpaQC2c1xA-unsplash.jpg",
+      "coverWidth": 16,
+      "coverHeight": 9,
+      "date": "2021-12-01",
+      "categories": [
+        "sveltekit",
+        "markdown",
+        "svelte"
+      ]
+    },
+    {
+      "title": "Syntax highlighting with mdsvex",
+      "slug": "syntax-highlighting-example",
+      "excerpt": "This post shows you how syntax highlighting works here.",
+      "coverImage": "/images/linus-nylund-Q5QspluNZmM-unsplash.jpg",
+      "coverWidth": 16,
+      "coverHeight": 9,
+      "date": "2021-12-01",
+      "categories": [
+        "sveltekit",
+        "web",
+        "css",
+        "markdown"
+      ]
+    },
+    {
+      "title": "Automatic heading links in mdsvex",
+      "slug": "heading-links-example",
+      "excerpt": "Check out how heading links work with this starter in this post.",
+      "coverImage": "/images/jefferson-santos-fCEJGBzAkrU-unsplash.jpg",
+      "coverWidth": 16,
+      "coverHeight": 9,
+      "date": "2021-10-26",
+      "categories": [
+        "sveltekit",
+        "markdown"
+      ]
+    },
+    {
+      "title": "A Markdown post with a Svelte component",
+      "slug": "mdsvex-component-example",
+      "excerpt": "This post demonstrates how to include a Svelte component in a Markdown post.",
+      "coverImage": "/images/jerry-zhang-ePpaQC2c1xA-unsplash.jpg",
+      "coverWidth": 16,
+      "coverHeight": 9,
+      "date": "2021-12-01",
+      "categories": [
+        "sveltekit",
+        "markdown",
+        "svelte"
+      ]
+    },
+    {
+      "title": "Syntax highlighting with mdsvex",
+      "slug": "syntax-highlighting-example",
+      "excerpt": "This post shows you how syntax highlighting works here.",
+      "coverImage": "/images/linus-nylund-Q5QspluNZmM-unsplash.jpg",
+      "coverWidth": 16,
+      "coverHeight": 9,
+      "date": "2021-12-01",
+      "categories": [
+        "sveltekit",
+        "web",
+        "css",
+        "markdown"
+      ]
+    },
+    {
+      "title": "Automatic heading links in mdsvex",
+      "slug": "heading-links-example",
+      "excerpt": "Check out how heading links work with this starter in this post.",
+      "coverImage": "/images/jefferson-santos-fCEJGBzAkrU-unsplash.jpg",
+      "coverWidth": 16,
+      "coverHeight": 9,
+      "date": "2021-10-26",
+      "categories": [
+        "sveltekit",
+        "markdown"
+      ]
+    },
+    {
+      "title": "A Markdown post with a Svelte component",
+      "slug": "mdsvex-component-example",
+      "excerpt": "This post demonstrates how to include a Svelte component in a Markdown post.",
+      "coverImage": "/images/jerry-zhang-ePpaQC2c1xA-unsplash.jpg",
+      "coverWidth": 16,
+      "coverHeight": 9,
+      "date": "2021-12-01",
+      "categories": [
+        "sveltekit",
+        "markdown",
+        "svelte"
+      ]
+    },
+    {
+      "title": "Syntax highlighting with mdsvex",
+      "slug": "syntax-highlighting-example",
+      "excerpt": "This post shows you how syntax highlighting works here.",
+      "coverImage": "/images/linus-nylund-Q5QspluNZmM-unsplash.jpg",
+      "coverWidth": 16,
+      "coverHeight": 9,
+      "date": "2021-12-01",
+      "categories": [
+        "sveltekit",
+        "web",
+        "css",
+        "markdown"
+      ]
+    },
+    {
+      "title": "Automatic heading links in mdsvex",
+      "slug": "heading-links-example",
+      "excerpt": "Check out how heading links work with this starter in this post.",
+      "coverImage": "/images/jefferson-santos-fCEJGBzAkrU-unsplash.jpg",
+      "coverWidth": 16,
+      "coverHeight": 9,
+      "date": "2021-10-26",
+      "categories": [
+        "sveltekit",
+        "markdown"
+      ]
+    },
+    {
+      "title": "A Markdown post with a Svelte component",
+      "slug": "mdsvex-component-example",
+      "excerpt": "This post demonstrates how to include a Svelte component in a Markdown post.",
+      "coverImage": "/images/jerry-zhang-ePpaQC2c1xA-unsplash.jpg",
+      "coverWidth": 16,
+      "coverHeight": 9,
+      "date": "2021-12-01",
+      "categories": [
+        "sveltekit",
+        "markdown",
+        "svelte"
+      ]
+    },
+    {
+      "title": "Syntax highlighting with mdsvex",
+      "slug": "syntax-highlighting-example",
+      "excerpt": "This post shows you how syntax highlighting works here.",
+      "coverImage": "/images/linus-nylund-Q5QspluNZmM-unsplash.jpg",
+      "coverWidth": 16,
+      "coverHeight": 9,
+      "date": "2021-12-01",
+      "categories": [
+        "sveltekit",
+        "web",
+        "css",
+        "markdown"
+      ]
+    },
+    {
+      "title": "Automatic heading links in mdsvex",
+      "slug": "heading-links-example",
+      "excerpt": "Check out how heading links work with this starter in this post.",
+      "coverImage": "/images/jefferson-santos-fCEJGBzAkrU-unsplash.jpg",
+      "coverWidth": 16,
+      "coverHeight": 9,
+      "date": "2021-10-26",
+      "categories": [
+        "sveltekit",
+        "markdown"
+      ]
+    },
+    {
+      "title": "A Markdown post with a Svelte component",
+      "slug": "mdsvex-component-example",
+      "excerpt": "This post demonstrates how to include a Svelte component in a Markdown post.",
+      "coverImage": "/images/jerry-zhang-ePpaQC2c1xA-unsplash.jpg",
+      "coverWidth": 16,
+      "coverHeight": 9,
+      "date": "2021-12-01",
+      "categories": [
+        "sveltekit",
+        "markdown",
+        "svelte"
+      ]
+    },
+    {
+      "title": "Syntax highlighting with mdsvex",
+      "slug": "syntax-highlighting-example",
+      "excerpt": "This post shows you how syntax highlighting works here.",
+      "coverImage": "/images/linus-nylund-Q5QspluNZmM-unsplash.jpg",
+      "coverWidth": 16,
+      "coverHeight": 9,
+      "date": "2021-12-01",
+      "categories": [
+        "sveltekit",
+        "web",
+        "css",
+        "markdown"
+      ]
+    },
+    {
+      "title": "Automatic heading links in mdsvex",
+      "slug": "heading-links-example",
+      "excerpt": "Check out how heading links work with this starter in this post.",
+      "coverImage": "/images/jefferson-santos-fCEJGBzAkrU-unsplash.jpg",
+      "coverWidth": 16,
+      "coverHeight": 9,
+      "date": "2021-10-26",
+      "categories": [
+        "sveltekit",
+        "markdown"
+      ]
+    },
+    {
+      "title": "A Markdown post with a Svelte component",
+      "slug": "mdsvex-component-example",
+      "excerpt": "This post demonstrates how to include a Svelte component in a Markdown post.",
+      "coverImage": "/images/jerry-zhang-ePpaQC2c1xA-unsplash.jpg",
+      "coverWidth": 16,
+      "coverHeight": 9,
+      "date": "2021-12-01",
+      "categories": [
+        "sveltekit",
+        "markdown",
+        "svelte"
+      ]
+    },
+    {
+      "title": "Syntax highlighting with mdsvex",
+      "slug": "syntax-highlighting-example",
+      "excerpt": "This post shows you how syntax highlighting works here.",
+      "coverImage": "/images/linus-nylund-Q5QspluNZmM-unsplash.jpg",
+      "coverWidth": 16,
+      "coverHeight": 9,
+      "date": "2021-12-01",
+      "categories": [
+        "sveltekit",
+        "web",
+        "css",
+        "markdown"
+      ]
+    },
+    {
+      "title": "Automatic heading links in mdsvex",
+      "slug": "heading-links-example",
+      "excerpt": "Check out how heading links work with this starter in this post.",
+      "coverImage": "/images/jefferson-santos-fCEJGBzAkrU-unsplash.jpg",
+      "coverWidth": 16,
+      "coverHeight": 9,
+      "date": "2021-10-26",
+      "categories": [
+        "sveltekit",
+        "markdown"
+      ]
+    },
+    {
+      "title": "A Markdown post with a Svelte component",
+      "slug": "mdsvex-component-example",
+      "excerpt": "This post demonstrates how to include a Svelte component in a Markdown post.",
+      "coverImage": "/images/jerry-zhang-ePpaQC2c1xA-unsplash.jpg",
+      "coverWidth": 16,
+      "coverHeight": 9,
+      "date": "2021-12-01",
+      "categories": [
+        "sveltekit",
+        "markdown",
+        "svelte"
+      ]
+    },
+    {
+      "title": "Syntax highlighting with mdsvex",
+      "slug": "syntax-highlighting-example",
+      "excerpt": "This post shows you how syntax highlighting works here.",
+      "coverImage": "/images/linus-nylund-Q5QspluNZmM-unsplash.jpg",
+      "coverWidth": 16,
+      "coverHeight": 9,
+      "date": "2021-12-01",
+      "categories": [
+        "sveltekit",
+        "web",
+        "css",
+        "markdown"
+      ]
+    },
+    {
+      "title": "Automatic heading links in mdsvex",
+      "slug": "heading-links-example",
+      "excerpt": "Check out how heading links work with this starter in this post.",
+      "coverImage": "/images/jefferson-santos-fCEJGBzAkrU-unsplash.jpg",
+      "coverWidth": 16,
+      "coverHeight": 9,
+      "date": "2021-10-26",
+      "categories": [
+        "sveltekit",
+        "markdown"
+      ]
+    },
+    {
+      "title": "A Markdown post with a Svelte component",
+      "slug": "mdsvex-component-example",
+      "excerpt": "This post demonstrates how to include a Svelte component in a Markdown post.",
+      "coverImage": "/images/jerry-zhang-ePpaQC2c1xA-unsplash.jpg",
+      "coverWidth": 16,
+      "coverHeight": 9,
+      "date": "2021-12-01",
+      "categories": [
+        "sveltekit",
+        "markdown",
+        "svelte"
+      ]
+    },
+    {
+      "title": "Syntax highlighting with mdsvex",
+      "slug": "syntax-highlighting-example",
+      "excerpt": "This post shows you how syntax highlighting works here.",
+      "coverImage": "/images/linus-nylund-Q5QspluNZmM-unsplash.jpg",
+      "coverWidth": 16,
+      "coverHeight": 9,
+      "date": "2021-12-01",
+      "categories": [
+        "sveltekit",
+        "web",
+        "css",
+        "markdown"
+      ]
+    },
+    {
+      "title": "Automatic heading links in mdsvex",
+      "slug": "heading-links-example",
+      "excerpt": "Check out how heading links work with this starter in this post.",
+      "coverImage": "/images/jefferson-santos-fCEJGBzAkrU-unsplash.jpg",
+      "coverWidth": 16,
+      "coverHeight": 9,
+      "date": "2021-10-26",
+      "categories": [
+        "sveltekit",
+        "markdown"
+      ]
+    },
+    {
+      "title": "A Markdown post with a Svelte component",
+      "slug": "mdsvex-component-example",
+      "excerpt": "This post demonstrates how to include a Svelte component in a Markdown post.",
+      "coverImage": "/images/jerry-zhang-ePpaQC2c1xA-unsplash.jpg",
+      "coverWidth": 16,
+      "coverHeight": 9,
+      "date": "2021-12-01",
+      "categories": [
+        "sveltekit",
+        "markdown",
+        "svelte"
+      ]
+    },
+    {
+      "title": "Syntax highlighting with mdsvex",
+      "slug": "syntax-highlighting-example",
+      "excerpt": "This post shows you how syntax highlighting works here.",
+      "coverImage": "/images/linus-nylund-Q5QspluNZmM-unsplash.jpg",
+      "coverWidth": 16,
+      "coverHeight": 9,
+      "date": "2021-12-01",
+      "categories": [
+        "sveltekit",
+        "web",
+        "css",
+        "markdown"
+      ]
+    },
+    {
+      "title": "Automatic heading links in mdsvex",
+      "slug": "heading-links-example",
+      "excerpt": "Check out how heading links work with this starter in this post.",
+      "coverImage": "/images/jefferson-santos-fCEJGBzAkrU-unsplash.jpg",
+      "coverWidth": 16,
+      "coverHeight": 9,
+      "date": "2021-10-26",
+      "categories": [
+        "sveltekit",
+        "markdown"
+      ]
+    },
+
+  ]
 
   let sortedPosts = posts.sort((a, b) => new Date(b.date) - new Date(a.date))
   
